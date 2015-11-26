8622340be61e6387bb286abce1cd9aa4c72cebe7
lienze <lienze2010@126.com>
2015-11-26 18:06:55 +0800

    继续完善程序，实现双击打开相应文档

--- WordTools/ExecWord.py
+++ WordTools/ExecWord.py
@@ -13,6 +13,7 @@ class ExecWord:
         self.word.Visible = 0
         self.filePath = filePath
         self.delegate = None
+        self.bFirstFindStr = False
 
     def findString(self, findstr):
         # self.word.Selection.Find.Wrap = 1
@@ -20,30 +21,40 @@ class ExecWord:
         # print self.word.Selection.Find.Text
         return boo_res
 
+    def openWordFile(self, wordPath=None):
+        print wordPath
+        self.word = Dispatch('word.application')
+        self.word.Visible = 1
+        self.word.Documents.Open(wordPath)
+
     def Exec(self, findstr):
 
         # 查找之前首先进行搜索结果列表清理
         self.delegate.treeWidget.clear()
         try:
             for path, dirs, files in os.walk(self.filePath):
+                self.bFirstFindStr = True
                 for filename in files:
                     if not (fnmatch.fnmatch(filename, '*.docx') or fnmatch.fnmatch(filename, '*.doc')):
                         continue
                     doc = os.path.abspath(os.path.join(path, filename))
                     # 状态栏显示相应进度
                     self.delegate.statusBar().showMessage(doc)
-                    # print 'processing %s...' % doc
+                    print 'processing %s...' % doc
                     self.word.Documents.Open(doc)
                     # docastext = doc[:-4] + 'txt'
                     # self.word.ActiveDocument.SaveAs(docastext, FileFormat=constants.wdFormatText)
+
                     if self.findString(findstr):
                         # print u'找到了'
-                        item = QtGui.QTreeWidgetItem(self.delegate.treeWidget)
-                        item.setText(0, doc)
+                        if self.bFirstFindStr:
+                            item = QtGui.QTreeWidgetItem(self.delegate.treeWidget)
+                            item.setText(0, path)
+                            self.bFirstFindStr = False
                         parameterItem = QtGui.QTreeWidgetItem(item)
-                        parameterItem.setText(0, "Parameter")
+                        parameterItem.setText(0, filename)
                     self.word.ActiveDocument.Close()
         finally:
-            self.word.Quit()
+            # self.word.Quit()
             print 'end'
 
