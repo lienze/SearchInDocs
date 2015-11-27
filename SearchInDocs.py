6b615760fa7283c3840fd646056c7127b7f580b1
lienze <lienze2010@126.com>
2015-11-27 18:40:46 +0800

    添加异常捕获

--- SearchInDocs.py
+++ SearchInDocs.py
@@ -1,4 +1,5 @@
 __author__ = 'lienze'
+# coding=utf-8
 import sys
 from PyQt4 import QtGui
 from UIControl import MainWindow
@@ -6,9 +7,13 @@ from UIControl import MainWindow
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
 
-    mp = MainWindow.Ui_MainWindow()
-    mp.show()
-
+    # 捕获所有异常并打印消息
+    try:
+        mp = MainWindow.Ui_MainWindow()
+        mp.show()
+    except Exception, e:
+        print e.message
     sys.exit(app.exec_())
+
 else:
     pass
